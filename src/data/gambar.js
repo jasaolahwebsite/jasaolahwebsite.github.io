c6ab67af981714ed7528f0f30ce790e58345845e
mzaini30 <muhzaini30@gmail.com>
2023-05-01 15:32:06 +0800

    Upload dari Termux

--- src/data/gambar.js
+++ src/data/gambar.js
@@ -1,4 +1,7 @@
 export default [
   "https://pbs.twimg.com/media/Fu65zJDaIAAW-xA?format=png&name=small",
   "https://pbs.twimg.com/media/Fu66dQoagAMG5Vt?format=png&name=small",
+  "https://pbs.twimg.com/media/FvBZaDDaMAAWzaP?format=png&name=small",
+  "https://pbs.twimg.com/media/FvBZWriaQAIZTAj?format=png&name=small",
+  "https://pbs.twimg.com/media/FvBZSDMakAYmE28?format=png&name=small"
 ];
