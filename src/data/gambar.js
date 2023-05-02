9e13f2e88b1373cd6a54a2078f426c08babe6a71
mzaini30 <muhzaini30@gmail.com>
2023-05-02 08:58:05 +0800

    Upload dari Termux

--- src/data/gambar.js
+++ src/data/gambar.js
@@ -1,7 +1,4 @@
 export default [
-  "https://pbs.twimg.com/media/Fu65zJDaIAAW-xA?format=png&name=small",
-  "https://pbs.twimg.com/media/Fu66dQoagAMG5Vt?format=png&name=small",
-  "https://pbs.twimg.com/media/FvBZaDDaMAAWzaP?format=png&name=small",
   "https://pbs.twimg.com/media/FvBZWriaQAIZTAj?format=png&name=small",
   "https://pbs.twimg.com/media/FvBZSDMakAYmE28?format=png&name=small"
 ];
